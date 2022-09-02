3cdc7608bac1405f575f8d99395069499ddad9d4
Alex <asacaquirin17@gmail.com>
2022-09-01 23:09:56 -0400

    Some user service done

--- src/main/java/Service/UserService.java
+++ src/main/java/Service/UserService.java
@@ -1,4 +1,12 @@
 package Service;
 
+import Models.User;
+import Repository.UserRepository;
+
 public class UserService {
+    private final UserRepository userRepository = new UserRepository();
+
+    public User createUser(User user){
+        return userRepository.create(user);
+    }
 }
